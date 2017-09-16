173f6db91e65757e38ed315b2eb0266a18857e37
ancashoria <horia.ancas@gmail.com>
2017-09-16 18:59:31 +0300

    hey. Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # graphql-kafka-subscriptions
+
+## Usage
+```
+import { KafkaPubSub } from 'graphql-kafka-subscriptions'
+
+export const pubsub = new KafkaPubSub({
+  topic: 'anything',
+  host: 'INSERT_KAFKA_IP',
+  port: 'INSERT_KAFKA_PORT',
+})
+```
+
+WIP
---
+++ src/kafka-pubsub.ts
@@ -0,0 +1,89 @@
+import * as Kafka from 'node-rdkafka'
+import { PubSubEngine } from 'graphql-subscriptions'
+import { PubSubAsyncIterator } from './pubsub-async-iterator'
+
+export interface IKafkaOptions {
+  topic: string,
+  host: string,
+  port: string,
+}
+
+export interface IKafkaTopic {
+  readStream: any,
+  writeStream: any,
+}
+
+export class KafkaPubSub implements PubSubEngine {
+  protected producer: any
+  protected consumer: any
+  protected options: any
+  protected subscriptionMap: { [subId: number]: [string, Function] }
+  protected channelSubscriptions: { [channel: string]: Array<number> }
+
+  constructor(options: IKafkaOptions) {
+    this.options = options
+    this.subscriptionMap = {}
+    this.channelSubscriptions = {}
+    this.producer = this.createProducer(this.options.topic)
+    this.consumer = this.createConsumer(this.options.topic)
+
+    this.consumer.on('data', (message) => {
+      console.log('Got message');
+      this.onMessage(JSON.parse(message.value.toString()))
+    });
+  }
+
+  public publish(payload) {
+    return this.producer.write(new Buffer(JSON.stringify(payload)))
+  }
+
+  public subscribe(channel: string, onMessage: Function, options: Object): Promise<number> {
+    const index = Object.keys(this.subscriptionMap).length
+    this.subscriptionMap[index] = [channel, onMessage]
+    this.channelSubscriptions[channel] = [
+      ...(this.channelSubscriptions[channel] || []), index
+    ]
+    return Promise.resolve(index)
+  }
+
+  public unsubscribe(index: number) {
+    const [channel] = this.subscriptionMap[index]
+    this.channelSubscriptions[channel] = this.channelSubscriptions[channel].filter(subId => subId !== index)
+  }
+
+  public asyncIterator<T>(triggers: string | string[]): AsyncIterator<T> {
+    return new PubSubAsyncIterator<T>(this, triggers);
+  }
+
+  private onMessage({channel, ...message}) {
+    const subscriptions = this.channelSubscriptions[channel]
+    if (!subscriptions) { return } // no subscribers, don't publish msg
+    for (const subId of subscriptions) {
+      const [cnl, listener] = this.subscriptionMap[subId]
+      listener(message)
+    }
+  }
+
+  private createProducer(topic: string) {
+    const producer = Kafka.Producer.createWriteStream({
+      'metadata.broker.list': `${this.options.host}:${this.options.port}`
+    }, {}, {topic})
+    producer.on('error', (err) => {
+      console.error('Error in our kafka stream')
+      console.error(err)
+    })
+    return producer
+  }
+
+  private createConsumer(topic: string) {
+    // Create a group for each instance. The consumer will receive all messages from the topic
+    const randomGroupId = Math.ceil(Math.random() * 9999)
+    const consumer = Kafka.KafkaConsumer.createReadStream({
+      'group.id': `kafka-group-${randomGroupId}`,
+      'metadata.broker.list': `${this.options.host}:${this.options.port}`,
+    }, {}, {
+      topics: [topic]
+    });
+    return consumer
+  }
+}
